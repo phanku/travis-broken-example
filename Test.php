739ef2e5920244f7e90a49ec714e36c6835e9526
Joe Pahl <phanku@vt.edu>
2017-05-30 16:54:47 -0400

    test

--- Test.php
+++ Test.php
@@ -2,9 +2,9 @@
 
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function testOnePlusOne() {
-		$this->assertEquals(1+1,1);
-  	}
+	// public function testOnePlusOne() {
+	// 	$this->assertEquals(1+1,1);
+ //  	}
 
   	public function testTwoPlusTwo() {
   		$this->assertEquals(2+2, 4);
